03bd2005212011a28ecb92bd4fcc72ad582d490b
Olivia <oliviarudolf67@gmail.com>
2022-09-05 23:56:12 +0200

    modifs article

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const Article = () => {
+    const { id } = useParams();
+    const [currentArticle, setCurrentArticle] = useState();
+
+    useEffect(() => {
+        fetch("http://localhost:3000/properties/" + id )
+          .then((res) => res.json())
+          .then((data) => {
+            setCurrentArticle(data);
+            console.log(data);
+            })
+          .catch((error) => console.error(error));
+      },[]);
+
+
+
     return (
         <div>
-            
+            {currentArticle}
         </div>
     );
 };
